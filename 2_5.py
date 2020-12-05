a09873f2a07f8e1e6bc56b6a8c93d50c944637ea
DKCisco <73546877+DKCisco@users.noreply.github.com>
2020-12-04 17:40:58 -0700

    Add files via upload

---
+++ 2_5.py
@@ -0,0 +1,24 @@
+"""
+5. Distance Traveled
+Assuming there are no accidents or delays, the distance that a car travels down the interstate can be calculated with the following formula:
+Distance 5 Speed 3 Time
+A car is traveling at 70 miles per hour. Write a program that displays the following:
+• The distance the car will travel in 6 hours
+• The distance the car will travel in 10 hours
+• The distance the car will travel in 15 hours
+
+"""
+
+# Store speed in variable
+speed = 70
+
+# Display the miles traveled in 6 hours
+print("In 6 hours, the car will travel " + str( (speed * 6)))
+
+# Display the miles traveled in 10 hours
+print("In 10 hours, the car will travel " + str( (speed * 10)))
+
+# Display the miles traveled in 15 hours
+print("In 15 hours, the car will travel " + str( (speed * 15)))
+
+
