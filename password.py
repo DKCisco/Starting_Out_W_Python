d9c96d4872f413f416d2e90545480befc2746d46
DKCisco <73546877+DKCisco@users.noreply.github.com>
2020-12-08 09:00:59 -0700

    Add files via upload

---
+++ password.py
@@ -0,0 +1,10 @@
+# This program compares two strings
+# Get a password from user
+password = input('Enter the password: ')
+
+# Determine whether the correct password
+# was entered
+if password == 'prospero':
+    print('Password Accepted.')
+else:
+    print('Sorry, that is the wrong password.')
