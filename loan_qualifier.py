f522a304d2340bb367e29f62d311cf8dd28656a0
DKCisco <73546877+DKCisco@users.noreply.github.com>
2020-12-10 15:31:33 -0700

    Add files via upload

---
+++ loan_qualifier.py
@@ -0,0 +1,25 @@
+# This program determines whether a bank customer
+# qualifies for a loan
+
+MIN_SALARY = 30000 # The minimum annual salary
+MIN_YEARS = 2      # The minimum years on the job
+
+# Get the customer's annual salary.
+salary = float(input('Enter your annual salary: '))
+
+# Get the number of years on the current job
+years_on_job = int(input('Enter the number of ' +
+                         ' years employed: ' ))
+
+# Determine whether the customer qualifies.
+if salary >= MIN_SALARY:
+    if years_on_job >= MIN_YEARS:
+        print('You qualify for the loan.')
+    else:
+        print('You must have been employed',
+              'for at least', MIN_YEARS,
+              'years to qualify.')
+else:
+    print('You must earn at least $',
+          format(MIN_SALARY, ',.2f'),
+          ' per year to qualify.', sep='')
