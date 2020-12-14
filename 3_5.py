c31fbc6073b754a112be602f9aadc7d354f820fd
DKCisco <73546877+DKCisco@users.noreply.github.com>
2020-12-14 07:26:42 -0700

    Add files via upload

---
+++ 3_5.py
@@ -0,0 +1,13 @@
+"""
+5. Write nested decision structures that perform the following: If amount1 is greater than
+10 and amount2 is less than 100, display the greater of amount1 and amount2
+
+"""
+
+# Input
+amount_1 = int(input('Enter numeric value for the first amount: '))
+amount_2 = int(input('Enter numeric value for the second amount: '))
+
+# Process
+if amount_1 > 10 and amount_2 < 100:
+    print(amount_1, amount_2)
