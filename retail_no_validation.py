903825220d7a45f61df2dbac2c835d56df13f4d5
DKCisco <73546877+DKCisco@users.noreply.github.com>
2021-02-25 08:50:01 -0700

    Add files via upload

---
+++ retail_no_validation.py
@@ -0,0 +1,21 @@
+# This program calculates retail prices with no validation.
+MARK_UP = 2.5 # The markup percentage
+another = 'y' # Variable to control the loop.
+
+# Process one or more items.
+while another == 'y' or another == 'Y':
+    # Get the item's wholesale cost.
+    wholesale = float(input("Enter the item's" +
+                            "wholesale cost: "))
+
+
+    # Calculate the retail price.
+    retail = wholesale * MARK_UP
+
+    # Display the retail price.
+    print('Retail price: $', format(retail, ',.2f'), sep='')
+
+
+    # Do this again?
+    another = input('Do you have another item? '+
+                    '(Enter y for yes): ')
